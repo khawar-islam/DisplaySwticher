1c3462c1c112a58f760156250604ef2a607166e0
MacBook Pro <khawar512@gmail.com>
2018-01-07 19:13:34 +0500

    correct code

---
+++ DisplaySwitcherXcode9/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DisplaySwitcherXcode9
+//
+//  Created by MacBook Pro on 07/01/2018.
+//  Copyright © 2018 MacBook Pro. All rights reserved.
+//
+
+import UIKit
+import DisplaySwitcher
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-DisplaySwitcherXcode9/Pods-DisplaySwitcherXcode9-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DisplaySwitcherXcode9 : NSObject
+@end
+@implementation PodsDummy_Pods_DisplaySwitcherXcode9
+@end
